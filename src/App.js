b6cfa503ab75ed8497cd94daa982b0f2f5643230
m_emin <mehmeteminbul@gmail.com>
2022-03-30 22:14:42 +0300

    session 1 ended button comp created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>TASK TRACKER</h1>
+      <Header title="TASK TRACKER" />
     </div>
   );
 }
